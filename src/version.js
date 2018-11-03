ddfd7adc085b1403be2743d5ed94026602cb4445
odf <olaf.delgado@gmail.com>
2018-11-03 16:55:20 +1100

    first usable library version; start removing unused files

--- src/version.js
+++
@@ -1,2 +0,0 @@
-export const gitRev = "223ff8bafa20f6188a9f6e8acd0968dfbd56e34b";
-export const gitDate = "2018-11-03 16:06:15 +1100";
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,22 @@
-var fs = require('fs');
 var path = require('path');
-var execSync = require('child_process').execSync;
-
-var basedir = __dirname;
-
-var execOpts = { encoding: 'utf8' };
-var gitRev = execSync('git rev-parse HEAD', execOpts);
-var gitDate = execSync('git show -s --format=%ci ${GIT_REV}', execOpts);
 
 
-fs.writeFileSync(path.join(basedir, 'src', 'version.js'),
-                 'export const gitRev = "' + gitRev.trim() + '";\n' +
-                 'export const gitDate = "' + gitDate.trim() + '";\n');
-
-
-module.exports = [
-  {
-    context: path.join(basedir, "src"),
-    entry: ["babel-polyfill", "./scripts/systreKey"],
-    output: {
-      path: path.join(basedir, "public", "js"),
-      filename: "systreKey.js"
-    },
-    module: {
-      rules: [
-        { test: /\.js$/,
-          exclude: /node_modules/,
-          use: "babel-loader" }
-      ]
-    },
-    resolve: {
-      extensions: [ ".js" ]
-    }
+module.exports = {
+  entry: ['babel-polyfill', './src/index'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'systreKey.js',
+    library: 'systreKey',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader' }
+    ]
+  },
+  resolve: {
+    extensions: [ '.js' ]
   }
-];
+}
