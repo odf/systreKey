02b119c0178437517b11269c359161a93a1c919f
odf <olaf.delgado@gmail.com>
2018-11-04 15:46:30 +1100

    no longer include babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index'],
+  entry: './src/index',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'systreKey.js',
